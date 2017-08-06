c0a333edae4d96f55d109103d2617b0b27ad2072
jchanghong <3200601@qq.com>
2017-08-06 09:52:36 +0800

    init

--- src/test/java/com/jchanghong/code/Java09_QueueWithTwoStacks.java
+++ src/test/java/com/jchanghong/code/Java09_QueueWithTwoStacks.java
@@ -10,7 +10,60 @@
  */
 package com.jchanghong.code;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.*;
+
 public class Java09_QueueWithTwoStacks {
-    public static void main(String[] args) {
+    static class MyQuene<T> extends AbstractQueue<T> {
+
+        private Stack<T> stack1 = new Stack<>();
+        private Stack<T> stack2 = new Stack<>();
+
+        @Override
+        public Iterator<T> iterator() {
+            return null;
+        }
+
+        @Override
+        public int size() {
+            return 0;
+        }
+
+        //需要实现-----------------------
+        @Override
+        public boolean offer(T t) {
+            return false;
+        }
+        //需要实现-----------------------
+        @Override
+        public T poll() {
+            return null;
+        }
+
+        @Override
+        public T peek() {
+            return null;
+        }
+    }
+    public Queue<Integer> getQuene() {
+        Queue<Integer> queue = new MyQuene<>();
+        //实现下面那个函数
+//        queue.poll();
+//        queue.offer(1);
+        return queue;
+    }
+
+    @Test
+    public void test() throws Exception {
+        Queue<Integer> queue = getQuene();
+        for (int i=0;i<1000;i++) {
+            queue.offer(i);
+        }
+        for (int i=0;i<1000;i++) {
+            Assert.assertEquals(queue.poll().intValue(), i);
+        }
+
     }
 }
--- src/test/java/com/jchanghong/code/Java10_Fibonacci.java
+++ src/test/java/com/jchanghong/code/Java10_Fibonacci.java
@@ -9,7 +9,33 @@
  */
 package com.jchanghong.code;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+/*
+* 在数学上，费波那契数列是以递归的方法来定义：
+F_{0}=0
+F_{1}=1
+F_{n}=F_{{n-1}}+F_{{n-2}}（n≧2）*/
 public class Java10_Fibonacci {
-    public static void main(String[] args) {
+    public int get(int n) {
+        return 0;
+    }
+
+    @Test
+    public void test() throws Exception {
+        for (int i=1;i<100;i++) {
+            Assert.assertEquals(get(i),testfunction(i));
+        }
+    }
+
+    public int testfunction(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else   {
+            return testfunction(n - 2) + testfunction(n - 1);
+        }
     }
 }
