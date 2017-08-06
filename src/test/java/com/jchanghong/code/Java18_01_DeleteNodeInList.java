39bb8d4fb7737133814ddf7610180799558ebcdf
jchanghong <3200601@qq.com>
2017-08-06 10:26:44 +0800

    init

--- src/test/java/com/jchanghong/code/Java18_01_DeleteNodeInList.java
+++ src/test/java/com/jchanghong/code/Java18_01_DeleteNodeInList.java
@@ -10,7 +10,22 @@
  */
 package com.jchanghong.code;
 
+import com.jchanghong.code.util.LinkNode;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Java18_01_DeleteNodeInList {
-    public static void main(String[] args) {
+
+    public boolean removeMe(LinkNode head, LinkNode me) {
+        return false;
+    }
+    @Test
+    public void test() throws Exception {
+        LinkNode head = new LinkNode(1, null);
+        head.next = new LinkNode(2, null);
+        head.next.next = new LinkNode(3, null);
+        Assert.assertEquals(removeMe(head, null), false);
+        Assert.assertEquals(removeMe(head, head.next), true);
+        Assert.assertEquals(head.next.values.intValue(), 3);
     }
 }
