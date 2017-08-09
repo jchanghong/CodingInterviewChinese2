73036579fb80c1a0b85c14fb3cbb041db4bcf701
jchanghong <3200601@qq.com>
2017-08-09 18:49:32 +0800

    update code

--- src/test/java/com/jchanghong/code/Java56_02_NumberAppearingOnce.java
+++ src/test/java/com/jchanghong/code/Java56_02_NumberAppearingOnce.java
@@ -6,11 +6,22 @@
  * <p>
  * // 面试题56（二）：数组中唯一只出现一次的数字
  * // 题目：在一个数组中除了一个数字只出现一次之外，其他数字都出现了三次。请
- * // 找出那个吃出现一次的数字。
+ * // 找出那个只出现一次的数字。
  */
 package com.jchanghong.code;
 
-public class Java56_02_NumberAppearingOnce {
-    public static void main(String[] args) {
+import com.jchanghong.code.util.UtilAssert;
+import org.junit.Test;
+
+public class Java56_02_NumberAppearingOnce extends UtilAssert{
+
+    public int once(int[] ints) {
+        return -1;
+    }
+
+    @Test
+    public void test() throws Exception {
+        int[] ints = {1, 2, 2, 2};
+       eq (once(ints), 1);
     }
 }
