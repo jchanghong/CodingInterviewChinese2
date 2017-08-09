118fdd7cb97674336a80ceba295d92d61bae7a92
jchanghong <3200601@qq.com>
2017-08-09 19:42:36 +0800

    update code

--- src/test/java/com/jchanghong/code/Java43_NumberOf1.java
+++ src/test/java/com/jchanghong/code/Java43_NumberOf1.java
@@ -10,7 +10,17 @@
  */
 package com.jchanghong.code;
 
-public class Java43_NumberOf1 {
-    public static void main(String[] args) {
+import com.jchanghong.code.util.UtilAssert;
+import org.junit.Test;
+
+public class Java43_NumberOf1 extends UtilAssert{
+    int numberOf1(int n) {
+        return 0;
+    }
+
+    @Test
+    public void test() throws Exception {
+        eq(numberOf1(12), 5);
+        eq(numberOf1(10), 2);
     }
 }
