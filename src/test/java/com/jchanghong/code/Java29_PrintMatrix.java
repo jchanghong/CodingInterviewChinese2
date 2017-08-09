c730bb989a26653e6e57c1a13bd508989a1fbf5a
jchanghong <3200601@qq.com>
2017-08-09 21:15:19 +0800

    update code

--- src/test/java/com/jchanghong/code/Java29_PrintMatrix.java
+++ src/test/java/com/jchanghong/code/Java29_PrintMatrix.java
@@ -9,7 +9,21 @@
  */
 package com.jchanghong.code;
 
-public class Java29_PrintMatrix {
-    public static void main(String[] args) {
+import com.jchanghong.code.util.UtilAssert;
+import org.junit.Test;
+
+public class Java29_PrintMatrix extends UtilAssert{
+    //元素之间没有任何其他符号
+    String print(int[][] ints) {
+        return "";
+    }
+
+    @Test
+    public void test() throws Exception {
+        int[][] ints = {
+                {1, 2},
+                {3, 4}
+        };
+        eq(print(ints), "1243");
     }
 }
