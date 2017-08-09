ca7ff59acb8103f7a7113e0f933569c7e093817a
jchanghong <3200601@qq.com>
2017-08-09 20:47:41 +0800

    update code

--- src/test/java/com/jchanghong/code/util/TreeUtil.java
+++ src/test/java/com/jchanghong/code/util/TreeUtil.java
@@ -72,7 +72,7 @@ public class TreeUtil {
         int nextlinenumber = 0;
         while (queue.peek() != null) {
             node = queue.poll();
-            System.out.print(node.values);
+            System.out.print(node.values+" ");
             linenumber--;
             if (node.left != null) {
                 nextlinenumber++;
@@ -94,6 +94,91 @@ public class TreeUtil {
     public void testtree() throws Exception {
         TreeNode node = construct(Arrays.asList(1, 2, 3, 4, 5, 6,7,8));
         print(node);
+
+    }
+
+    /**
+     * @param ints 层次遍历，-1表示空节点。比如1，-1，-1 代表一个节点1
+     * @return
+     */
+    public TreeNode construct2(int... list) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        TreeNode treeNode = new TreeNode(list[0], null, null);
+        queue.offer(treeNode);
+        int index = 1;
+        while (queue.peek() != null && index < list.length) {
+            TreeNode me = queue.poll();
+            int left = list[index++];
+            if (left != -1) {
+                me.left = new TreeNode(left, null, null);
+                queue.offer(me.left);
+            }
+            if (index < list.length) {
+                int right = list[index++];
+                if (right != -1) {
+                    me.right = new TreeNode(right, null, null);
+                    queue.offer(me.right);
+                }
+            }
+        }
+        return treeNode;
+    }
+    /**
+     * @param head 层次遍历
+     */
+    public static void print2(TreeNode head) {
+        List<StringBuilder> strings = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<TreeNode> list = (List<TreeNode>) queue;
+        queue.offer(head);
+        TreeNode node;
+        int linenumber = 1;
+        int nextlinenumber = 0;
+        StringBuilder builder = new StringBuilder();
+        while (list.size() > 0) {
+            node = queue.poll();
+            if (node == null) {
+                builder.append("*  ");
+                linenumber--;
+                if (linenumber == 0) {
+                    for (StringBuilder string : strings) {
+                        string.insert(0, "  ");
+                    }
+                    builder.append("\n");
+                    strings.add(builder);
+                    builder = new StringBuilder();
+                    linenumber = nextlinenumber;
+                    nextlinenumber = 0;
+                }
+            }
+            else {
+                builder.append(node.values + "  ");
+                linenumber--;
+                nextlinenumber++;
+                queue.offer(node.left);
+                nextlinenumber++;
+                queue.offer(node.right);
+                if (linenumber == 0) {
+                    for (StringBuilder string : strings) {
+                        string.insert(0, "  ");
+                    }
+                    builder.append("\n");
+                    strings.add(builder);
+                    builder = new StringBuilder();
+                    linenumber = nextlinenumber;
+                    nextlinenumber = 0;
+                }
+            }
+
+        }
+        strings.forEach(a -> System.out.println(a.toString()));
+    }
+
+    @Test
+    public void testconst2() throws Exception {
+        TreeNode node = construct2(1, 2, 3, -1, -1, 7, 9, 10);
+        node = construct2(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
+        print2(node);
 
     }
 }
