4cbe14eeaffe09b5afbbdf26fb4870c03c4587a0
jchanghong <3200601@qq.com>
2017-08-10 14:37:19 +0800

    更新代码

--- src/test/java/com/jchanghong/code/Java24_ReverseList.java
+++ src/test/java/com/jchanghong/code/Java24_ReverseList.java
@@ -15,12 +15,26 @@ import com.jchanghong.code.util.LinkNode;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class Java24_ReverseList {
     public LinkNode reverseList(LinkNode head) {
-        LinkNode result = head;
-        return result;
+        Deque<LinkNode> nodes = new LinkedList<>();
+        while (head != null) {
+            nodes.addFirst(head);
+            head = head.next;
+        }
+        LinkNode pre = new LinkNode(0, null);
+        LinkNode stemp = pre;
+        while (nodes.peekFirst() != null) {
+            LinkNode next = nodes.pollFirst();
+            next.next = null;
+            pre.next = next;
+            pre = pre.next;
+        }
+        return stemp.next;
     }
-
     @Test
     public void test() throws Exception {
         LinkNode head = LinkListUtil.construct(1, 2, 3, 4, 5, 6);
