94ac0b04318340b8f72502839598752abf4715d6
jchanghong <3200601@qq.com>
2017-08-10 09:39:05 +0800

    更新代码

--- src/test/java/com/jchanghong/code/Java11_MinNumberInRotatedArray.java
+++ src/test/java/com/jchanghong/code/Java11_MinNumberInRotatedArray.java
@@ -15,8 +15,33 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class Java11_MinNumberInRotatedArray {
+    //3, 4, 5, 1, 2}为{1, 2, 3, 4, 5}的一个旋转，该数组的最小值为1。
     public int min(int[] ints) {
+        int start = 0;
+        int end = ints.length-1;
+        while (start <= end) {
+            if (start == end) {
+                return ints[start];
+            }
+            int m = (start + end) >> 2;
+
+            if (ints[m] < ints[end]) {
+                end = m-1;
+            } else if (ints[m] >ints[start]) {
+                start = m+1;
+            } else return min2(ints);
+        }
         return Integer.MIN_VALUE;
+    }
+
+    private int min2(int[] ints) {
+        int min = Integer.MAX_VALUE;
+        for (int anInt : ints) {
+            if (anInt < min) {
+                min = anInt;
+            }
+        }
+        return min;
     }
 
     @Test
