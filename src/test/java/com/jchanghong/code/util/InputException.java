ee91bbf771f885512218cd2b14faede2cb47afcb
jchanghong <3200601@qq.com>
2017-08-10 09:19:57 +0800

    更新代码

---
+++ src/test/java/com/jchanghong/code/util/InputException.java
@@ -0,0 +1,14 @@
+package com.jchanghong.code.util;
+
+/**
+ * \* Created with IntelliJ IDEA.
+ * \* User: jiang
+ * \* Date: 2017/8/10 0010
+ * \* Time: 9:18
+ * \
+ */
+public class InputException extends Exception {
+    public InputException(String message) {
+        super(message);
+    }
+}
