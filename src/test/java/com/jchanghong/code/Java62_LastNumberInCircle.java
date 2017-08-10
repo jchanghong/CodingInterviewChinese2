777729195ae00deca62e2c26cc5e5c5fb7ebaf0a
jchanghong <3200601@qq.com>
2017-08-10 16:19:35 +0800

    更新代码

--- src/test/java/com/jchanghong/code/Java62_LastNumberInCircle.java
+++ src/test/java/com/jchanghong/code/Java62_LastNumberInCircle.java
@@ -14,8 +14,26 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class Java62_LastNumberInCircle {
+
     public int lastNumber(int[] ints,int m) {
         return -1;
+    }
+
+    int next(int size, int m, int start) {
+        int next = start;
+        while (m > 0) {
+            next++;
+            if (next == size) {
+                next = 0;
+            }
+            m--;
+        }
+        return next;
+    }
+
+    @Test
+    public void testnext() throws Exception {
+        System.out.println(next(3, 1, 2));
     }
 
     @Test
