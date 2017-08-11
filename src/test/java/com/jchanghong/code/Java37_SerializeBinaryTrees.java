e93aa6800ecd5117cf9a8627e5d1693ebb41a741
jchanghong <3200601@qq.com>
2017-08-11 10:51:21 +0800

    更新代码

--- src/test/java/com/jchanghong/code/Java37_SerializeBinaryTrees.java
+++ src/test/java/com/jchanghong/code/Java37_SerializeBinaryTrees.java
@@ -14,17 +14,60 @@ import com.jchanghong.code.util.TreeUtil;
 import com.jchanghong.code.util.UtilAssert;
 import org.junit.Test;
 
-import java.util.Arrays;
+import java.io.StringReader;
+import java.util.*;
 
 public class Java37_SerializeBinaryTrees extends UtilAssert {
 
     String serialize(TreeNode head) {
+//        return print2(head);
         return "";
     }
 
     TreeNode deSerize(String string) {
         return null;
+//        List<Integer> list = new ArrayList<>();
+//        Scanner scanner = new Scanner(new StringReader(string));
+//        while (scanner.hasNext()) {
+//            int i = -1;
+//            try {
+//                i = scanner.nextInt();
+//                list.add(i);
+//            } catch (Exception e) {
+//            }
+//        }
+//        int[] ints = new int[list.size()];
+//        for (int i=0;i<list.size();i++) {
+//            ints[i] = list.get(i);
+//        }
+//        return TreeUtil.construct2(ints);
     }
+    /**
+     * @param head 层次遍历
+     */
+    public static String print2(TreeNode head) {
+        List<StringBuilder> strings = new ArrayList<>();
+        if (head == null) {
+            return "";
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(head);
+        TreeNode node;
+        StringBuilder builder = new StringBuilder();
+        while (queue.size() > 0) {
+            node = queue.poll();
+            if (node == null) {
+                builder.append("* ");
+            } else {
+                builder.append(node.values+" ");
+                queue.offer(node.left);
+                queue.offer(node.right);
+            }
+        }
+        return builder.toString();
+
+    }
+
 
     @Test
     public void test() throws Exception {
