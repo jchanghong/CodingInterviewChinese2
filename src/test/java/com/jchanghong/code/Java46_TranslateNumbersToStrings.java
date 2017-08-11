7ae8824fae10338a43ad3681526bd2c4a7d89a7b
jchanghong <3200601@qq.com>
2017-08-11 08:09:55 +0800

    更新代码

--- src/test/java/com/jchanghong/code/Java46_TranslateNumbersToStrings.java
+++ src/test/java/com/jchanghong/code/Java46_TranslateNumbersToStrings.java
@@ -14,10 +14,63 @@ package com.jchanghong.code;
 
 import com.jchanghong.code.util.UtilAssert;
 import org.junit.Test;
+import org.omg.CORBA.PUBLIC_MEMBER;
 
 public class Java46_TranslateNumbersToStrings extends UtilAssert {
+    //12258->
+    //1 2258
+    //12 258
     int number(int number) {
-        return 0;
+        if (number < 100) {
+            if (number <= 25)
+                return 2;
+            else
+                return 1;
+        }
+        TWO two = first2(number);
+        if (two.one <= 25) {
+            TWO two1 = firt1(number);
+            int number1 = number(two1.two);
+            int number2 = number(two.two);
+            return number1 + number2;
+        }
+        else {
+            return number(two.two);
+        }
+    }
+
+    char int2char(int i) {
+        return (char) (i +'a');
+    }
+
+    static class TWO {
+        public int one;
+        public int two;
+
+        public TWO(int one, int two) {
+            this.one = one;
+            this.two = two;
+        }
+    }
+    TWO firt1(int i) {
+        TWO two = new TWO(0, 0);
+        String s = String.valueOf(i);
+        two.one= Integer.valueOf(s.substring(0, 1));
+        two.two = Integer.valueOf(s.substring(1));
+        return two;
+    }
+    TWO first2(int i) {
+        TWO two = new TWO(0, 0);
+        String s = String.valueOf(i);
+        two.one= Integer.valueOf(s.substring(0, 2));
+        two.two = Integer.valueOf(s.substring(2));
+        return two;
+    }
+
+    @Test
+    public void test2() throws Exception {
+        isTrue(int2char(0)=='a');
+        isTrue(int2char(25)=='z');
     }
 
     @Test
