2978673e86cdf18f49a4c21c3a5575cf2b2f0113
jchanghong <3200601@qq.com>
2017-08-14 12:55:07 +0800

    更新代码

--- src/test/java/com/jchanghong/code/Java02_SingletonTest.java
+++ src/test/java/com/jchanghong/code/Java02_SingletonTest.java
@@ -6,7 +6,7 @@ package com.jchanghong.code;
 public class Java02_SingletonTest {
     private static final Java02_SingletonTest SINGLETON = new Java02_SingletonTest();
 
-    //不要public
+    //不要用public
     private Java02_SingletonTest() {
     }
 
